751cfe5589343b4b9953ef7a2c6d5cabd3f51429
Berava <berava@gmail.com>
2018-09-20 16:38:24 +0200

    Finish the Pass Down Search result and Render Result List assessment

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,26 @@ import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {name: 'Tiny Dancer',
+        artist: 'Elton John',
+        album: 'Madman Across The Water',
+        id: 1},
+        {name: 'Tiny Dancer',
+        artist: 'Tim McGraw',
+        album: 'Love Story',
+        id: 2},
+        {name: 'Tiny Dancer',
+        artist: 'Rockabye Baby!',
+        album: 'Lullaby renditions of Elton John',
+        id:3},
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +32,7 @@ export class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults} />
             <Playlist />
           </div>
         </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,6 +6,11 @@ export class TrackList extends React.Component {
   render() {
     return(
       <div className="TrackList">
+      {
+        this.props.tracks.map(track => {
+          return <Track track={track} />;
+        })
+      }
         // You will add a map method that renders a set of Track components
       </div>
     );
